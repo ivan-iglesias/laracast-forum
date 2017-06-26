09e8950180d03edd94631559f74e502e58741188
ivan-iglesias <iiglesiasbilbao@gmail.com>
2017-06-26 20:52:02 +0200

    Lesson 11

--- app/Channel.php
+++ app/Channel.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-    //
+    /**
+     * override
+     * Por defecto, el enlazado con el modelo de laravel intentara
+     * obtener el canal por su id, por lo que podemos sobreescribir
+     * el metodo e indicar que lo buque por el campo slug".
+     * $this->get('/threads/' . $channel->slug)
+     */
+    public function getRouteKeyName()
+    {
+    	return 'slug';
+    }
+
+    public function threads()
+    {
+    	return $this->hasMany(Thread::class);
+    }
 }
